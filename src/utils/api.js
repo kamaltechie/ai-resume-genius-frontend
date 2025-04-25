c0196c95707539c16cf4bb4418cd09763eca375c
kamaltechie <ezraidi.kamal.04@gmail.com>
2025-04-25 13:37:49 +0200

    front

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,6 @@ import { getSession } from 'next-auth/react';
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
-
 api.interceptors.request.use(async (config) => {
   const session = await getSession();
   if (session?.accessToken) {
